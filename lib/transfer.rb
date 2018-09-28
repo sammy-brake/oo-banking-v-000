f0e229eb91706f63cc9934638791390df29e68f5
Samantha brake <sammy.brake@gmail.com>
2018-09-28 15:07:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end 
   
   def execute_transaction
-<<<<<<< HEAD
+
    if @status == "pending" && valid? 
     sender.balance -= amount 
     receiver.balance += amount 
@@ -22,7 +22,7 @@ class Transfer
    "Transaction rejected. Please check your account balance."
    @status = "rejected"
    end 
-=======
+
    if @status == "pending" && self.valid? 
     sender.balance -= amount 
     receiver.balance += amount 
@@ -30,7 +30,7 @@ class Transfer
  else
    
  end 
->>>>>>> b9f45c57ab44ad8601613badc83da4bfe3be7ee0
+
   
   end 
   

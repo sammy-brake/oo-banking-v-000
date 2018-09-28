4c97212629098c7b5d811f26843da9682f643857
Samantha brake <sammy.brake@gmail.com>
2018-09-28 15:15:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,9 @@ class Transfer
     receiver.balance += amount 
     self.status = "complete"
    else
-   "Transaction rejected. Please check your account balance."
+   
    self.status = "rejected"
+   "Transaction rejected. Please check your account balance."
    end 
   end 
   
